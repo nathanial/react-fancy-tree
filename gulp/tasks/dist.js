2f889b45fe20a2adb7c894b4fc57e1b9abc4d77f
Nathan Hartman <nathanial.hartman@gmail.com>
2016-02-10 17:40:20 -0800

    created dist task

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -4,10 +4,10 @@ var less = require('gulp-less');
 var merge = require('merge-stream');
 var preprocess = require('gulp-preprocess');
 
-gulp.task('build', function(){
+gulp.task('dist', function(){
   return merge(
     gulp.src('src/**/*.js')
-        .pipe(preprocess({context: { NODE_ENV: 'development', DEBUG: true}}))
+        .pipe(preprocess({context: { NODE_ENV: 'production', DEBUG: true}}))
         .pipe(babel({
           stage: 0
         })).pipe(gulp.dest('build')),
